f03399e03b335fc2b330608a608350073b49be6d
addtobasic <addtobasic@gmail.com>
2024-10-22 04:37:34 +0900

    feat: ログイン機能の実装

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+// app/layout.tsx (または他のファイル)
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
+import SessionWrapper from './SessionWrapper'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -26,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <SessionWrapper>{children}</SessionWrapper>
       </body>
     </html>
   )
