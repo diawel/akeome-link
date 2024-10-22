6708f5596de60944dcc193c65154a10006b42bb4
addtobasic <addtobasic@gmail.com>
2024-10-22 18:57:09 +0900

    fix: router.pushからredirect()に修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import { useSession, signIn } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 const Login = () => {
   const { data: session, status } = useSession()
-  const router = useRouter()
 
-  useEffect(() => {
-    if (status === 'authenticated') {
-      router.push('/create/list')
-    }
-  }, [status, router])
+  if (status === 'authenticated') {
+    redirect('/create/list')
+  }
 
   return (
     <div>
