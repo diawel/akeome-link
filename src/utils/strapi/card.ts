708e85955f153a17e4e751ef256e16c68e875523
Diawel <38136957+diawel@users.noreply.github.com>
2024-10-26 12:00:07 +0900

    fix: strapiの既存のutilを整理

--- src/utils/strapi/card.ts
+++ src/utils/strapi/card.ts
@@ -1,5 +1,9 @@
-import { StrapiRecord } from '.'
+'use server'
+
+import { getServerSession } from 'next-auth'
+import { StrapiApiListResponse, StrapiError, StrapiRecord } from '.'
 import { MediaAttributes } from './media'
+import { authOptions } from '../../app/api/auth/[...nextauth]/authOptions'
 
 export type CardAttributes = {
   title: string
@@ -10,4 +14,79 @@ export type CardAttributes = {
   publishedAt: string
   userImages: { data: StrapiRecord<MediaAttributes>[] }
   creator: { data: StrapiRecord<{ id: string }> }
+}
+
+export const getCreatedCards = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return undefined
+  }
+
+  try {
+    const strapiResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/api/cards?populate=creator,userImages&filters[creator][id][$eq]=${session.user.strapiUserId}`,
+      { cache: 'no-cache' }
+    )
+
+    if (!strapiResponse.ok) {
+      const strapiError: StrapiError = await strapiResponse.json()
+      throw new Error(strapiError.error.message)
+    }
+
+    const cards: StrapiApiListResponse<CardAttributes> =
+      await strapiResponse.json()
+    return cards
+  } catch (error) {
+    throw error
+  }
+}
+
+export const addCard = async ({
+  title,
+  creatorName,
+  userImages,
+  layout,
+}: {
+  title: string
+  creatorName: string
+  userImages: {
+    id: string
+  }[]
+  layout: string
+}) => {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    throw new Error('Unauthorized')
+  }
+
+  const formData = new FormData()
+  formData.append('title', title)
+  formData.append('creatorName', creatorName)
+  userImages.forEach((image) => {
+    formData.append('userImages', image.id)
+  })
+  formData.append('layout', JSON.stringify(layout))
+  formData.append('creator', String(session.user.strapiUserId))
+
+  try {
+    const strapiResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/cards`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${session.strapiToken}`,
+        },
+        body: formData,
+      }
+    )
+
+    if (!strapiResponse.ok) {
+      throw new Error(`Failed to add card: ${strapiResponse.statusText}`)
+    }
+  } catch (error) {
+    console.error(error)
+    throw new Error('Error adding card')
+  }
 }
--- src/utils/strapi/sticker.ts
+++ src/utils/strapi/sticker.ts
@@ -1,4 +1,6 @@
-import { StrapiRecord } from '.'
+'use server'
+
+import { StrapiApiListResponse, StrapiRecord } from '.'
 import { MediaAttributes } from './media'
 
 export type StickerAttributes = {
@@ -7,5 +9,27 @@ export type StickerAttributes = {
   publishedAt: string
   image: {
     data: StrapiRecord<MediaAttributes>
+  }
+}
+
+export const getStickers = async () => {
+  try {
+    const strapiResponse = await fetch(
+      `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/api/stickers?populate=image`
+    )
+
+    const strapiData: StrapiApiListResponse<StickerAttributes> =
+      await strapiResponse.json()
+
+    const result = strapiData.data.map((item) => ({
+      id: item.id,
+      image: item.attributes.image
+        ? item.attributes.image.data.attributes
+        : null,
+    }))
+
+    return result
+  } catch (error) {
+    throw error
   }
 }
