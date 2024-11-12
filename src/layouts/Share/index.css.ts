891c558aec590b42da81c048c2025c56341f66eb
addtobasic <addtobasic@gmail.com>
2024-11-13 00:38:04 +0900

    add: ひとまずshare/idが動くように

---
+++ src/layouts/Share/index.css.ts
@@ -0,0 +1,166 @@
+import { style } from '@vanilla-extract/css'
+import { color } from '../../utils/styleSchema'
+
+export const postContainer = style({
+  position: 'relative',
+  width: '100%',
+  height: 'auto',
+})
+
+export const post = style({
+  width: '100%',
+  height: '288px',
+})
+
+export const card = style({
+  position: 'absolute',
+  marginTop: '180px',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  zIndex: 1,
+  width: '282px',
+  padding: '16px',
+})
+
+export const linkBoxContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingTop: '39px',
+})
+
+export const linkBoxContent = style({
+  display: 'flex',
+  alignItems: 'center',
+  border: `1px solid ${color.red[5]}`,
+  marginTop: '220px',
+  borderRadius: '8px',
+  overflow: 'hidden',
+  backgroundColor: color.gray[100],
+  width: '326px',
+  height: '46px',
+})
+
+export const linkText = style({
+  flex: 1,
+  padding: '10px 16px',
+  color: color.red[5],
+  fontSize: '13px',
+  textDecoration: 'none',
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+})
+
+export const copyButton = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundColor: color.red[5],
+  marginTop: '8px',
+  padding: '10px 12px',
+  cursor: 'pointer',
+  color: color.red[5],
+  fontSize: '14px',
+  fontWeight: 'bold',
+  border: 'none',
+})
+
+export const linkInput = style({
+  flex: 1,
+  padding: '10px 16px',
+  fontSize: '16px',
+  color: color.red[5],
+  border: 'none',
+  outline: 'none',
+  backgroundColor: 'transparent',
+  textOverflow: 'ellipsis',
+  fontWeight: 'bold',
+  cursor: 'default',
+  width: '320px',
+})
+
+export const xContainer = style({
+  width: '74px',
+  height: '56px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'white',
+  borderRadius: '8px',
+  cursor: 'pointer',
+})
+
+export const lineContainer = style({
+  width: '74px',
+  height: '56px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'white',
+  borderRadius: '8px',
+  cursor: 'pointer',
+})
+
+export const downloadContainer = style({
+  width: '145px',
+  height: '56px',
+  backgroundColor: 'white',
+  borderRadius: '8px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+  border: 'none',
+})
+
+export const downloadContent = style({
+  display: 'flex',
+  alignItems: 'center',
+})
+
+export const downloadText = style({
+  fontSize: '13px',
+  fontWeight: 'bold',
+  paddingLeft: '4px',
+})
+
+export const iconButtonContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingTop: '25px',
+})
+
+export const buttonSpace = style({
+  padding: '0 15px',
+})
+
+export const buttonContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingTop: '32px',
+  cursor: 'pointer',
+})
+
+export const buttonContent = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: color.red[5],
+  width: '127px',
+  height: '42px',
+  borderRadius: '12px',
+})
+
+export const buttonText = style({
+  fontSize: '16px',
+  color: 'white',
+  fontWeight: 'bold',
+})
+
+export const cardLink = style({
+  textDecoration: 'none',
+})
---
+++ src/layouts/Share/index.tsx
@@ -0,0 +1,130 @@
+'use client'
+
+import Card from '../../components/Card'
+import postImage from './post.svg'
+import xIcon from './icon-x.svg'
+import lineIcon from './icon-line.svg'
+import downloadIcon from './icon-download.svg'
+import Image from 'next/image'
+import * as styles from './index.css'
+import copyIcon from './icon-copy.svg'
+import Link from 'next/link'
+import { mediaRecordsToUrlSet } from '../../utils/strapi/strapiImage'
+import { StrapiRecord } from '../../utils/strapi'
+import Renderer from '../../components/Card/Renderer'
+import { useEffect, useState } from 'react'
+import { CardAttributes } from '../../utils/strapi/card'
+
+type ShareProps = {
+  cardRecord: StrapiRecord<CardAttributes>
+}
+
+const Share = ({ cardRecord }: ShareProps) => {
+  const [renderedImage, setRenderedImage] = useState<Blob | null>(null)
+  const [shareUrl, setShareUrl] = useState('')
+  useEffect(() => {
+    setShareUrl(
+      new URL(`/link/${cardRecord.attributes.shareId}`, window.location.href)
+        .href
+    )
+  }, [cardRecord.attributes.shareId])
+  return (
+    <>
+      <div>
+        <div className={styles.postContainer}>
+          <Image className={styles.post} src={postImage} alt="postImage" />
+          <div className={styles.card}>
+            <Card
+              layout={cardRecord.attributes.view.layout}
+              background={cardRecord.attributes.view.background}
+              userImages={mediaRecordsToUrlSet(
+                cardRecord.attributes.userImages.data
+              )}
+              randomVariants="hidden"
+            />
+          </div>
+        </div>
+
+        <div className={styles.linkBoxContainer}>
+          <div className={styles.linkBoxContent}>
+            <input
+              type="text"
+              value={shareUrl}
+              readOnly
+              className={styles.linkInput}
+            />
+            <button
+              className={styles.copyButton}
+              onClick={() => {
+                navigator.clipboard.writeText(shareUrl)
+                alert('コピーしました')
+              }}
+            >
+              <Image src={copyIcon} alt="copyIcon"></Image>
+            </button>
+          </div>
+        </div>
+        <div>
+          <div className={styles.iconButtonContainer}>
+            <Link
+              className={styles.xContainer}
+              href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+                shareUrl
+              )}&text=年賀状が届いています #あけおめリンク`}
+            >
+              <Image src={xIcon} alt="xIcon"></Image>
+            </Link>
+            <div className={styles.buttonSpace}>
+              <Link
+                className={styles.lineContainer}
+                href={`https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(
+                  shareUrl
+                )}&text=年賀状が届いています`}
+              >
+                <Image src={lineIcon} alt="lineIcon"></Image>
+              </Link>
+            </div>
+            <button
+              className={styles.downloadContainer}
+              onClick={() => {
+                if (!renderedImage) return
+                const url = URL.createObjectURL(renderedImage)
+                const a = document.createElement('a')
+                a.href = url
+                a.setAttribute('download', `new_year_card_${Date.now()}`)
+                document.body.appendChild(a)
+                a.click()
+                URL.revokeObjectURL(url)
+                a.remove()
+              }}
+              style={{
+                pointerEvents: renderedImage ? 'auto' : 'none',
+                opacity: renderedImage ? 1 : 0.5,
+              }}
+            >
+              <div className={styles.downloadContent}>
+                <Image src={downloadIcon} alt="downLoadIcon"></Image>
+                <div className={styles.downloadText}>画像として保存</div>
+              </div>
+            </button>
+          </div>
+          <Link href="/create/list" className={styles.cardLink}>
+            <div className={styles.buttonContainer}>
+              <div className={styles.buttonContent}>
+                <div className={styles.buttonText}>一覧に戻る</div>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+      <Renderer
+        layout={cardRecord.attributes.view.layout}
+        background={cardRecord.attributes.view.background}
+        userImages={mediaRecordsToUrlSet(cardRecord.attributes.userImages.data)}
+        onRender={setRenderedImage}
+      />
+    </>
+  )
+}
+
+export default Share
