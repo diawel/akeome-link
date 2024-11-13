67361c5bd9d75cc710652c8fa221b01d8b0712c6
addtobasic <addtobasic@gmail.com>
2024-11-14 02:25:44 +0900

    fix: UIをデザインに沿ったものにする

--- src/layouts/Share/Qr/index.tsx
+++ src/layouts/Share/Qr/index.tsx
@@ -1,8 +1,13 @@
 'use client'
 
-import { QRCodeCanvas } from 'qrcode.react';
-import Link from 'next/link'
+import { QRCodeCanvas } from 'qrcode.react'
 import { useEffect, useState } from 'react'
+import akeomeLinkLogo from './akeome-link-logo.svg'
+import qrBackgroundImage from './qr-background.svg' 
+import * as styles from './index.css'
+import { color } from '../../../utils/styleSchema'
+import Image from 'next/image'
+import ReturnButton from '../../../components/ReturnButton'
 
 type QrProps = {
   creatorName: string
@@ -20,18 +25,32 @@ const Qr = ({ creatorName, id }: QrProps) => {
   }, [id])
 
   return (
-    <div>
-      {shareUrl ? (
-        <div>
-          <QRCodeCanvas value={shareUrl} size={200} />
-          <p>{shareUrl}</p>
-          <p>{creatorName}</p>
+    <>
+      <div className={styles.backgroundImage}>
+        <div className={styles.textOverlay}>
+          <div className={styles.returnButtonSpace}>
+            <div className={styles.returnButton}>
+              <ReturnButton href="/create/list" color={color.gray[100]} />
+            </div>
+          </div>
+          <div>
+            <Image src={akeomeLinkLogo} alt='akeomeLinkLogo' />
+            <div className={styles.qrCodeSpace}>
+              <div className={styles.qrCode}>
+                <QRCodeCanvas value={shareUrl} size={215} />
+              </div>
+            </div>
+            <div className={styles.creatorName}>
+              <div>{creatorName}</div>
+            </div>
+            <div className={styles.discriptionText}>から年賀状を受け取ろう！</div>
+          </div>
         </div>
-      ) : (
-        <p>urlなし</p>
-      )}
-    </div>
+        <Image className={styles.image} src={qrBackgroundImage} alt="qrBackgroundImage" />
+      </div>
+    </>
   );
 }
 
 export default Qr
+
