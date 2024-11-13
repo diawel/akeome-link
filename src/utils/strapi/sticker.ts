80b03b809afb09e233bf24edca0d8d9e550052db
Diawel <38136957+diawel@users.noreply.github.com>
2024-11-14 02:14:51 +0900

    add: ステッカーを全件取得する

--- src/utils/strapi/sticker.ts
+++ src/utils/strapi/sticker.ts
@@ -19,22 +19,39 @@ export type StickerAttributes = {
 
 export const getStickers = async () => {
   try {
-    const strapiResponse = await fetch(
-      `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/api/stickers?${stringify({
-        populate: ['image', 'randomVariants'],
-      })}`,
-      {
-        cache: 'no-cache',
-        headers: {
-          Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
-        },
+    const stickerRecords: StrapiRecord<StickerAttributes>[] = []
+    const getPage = async (page: number) => {
+      const strapiResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_STRAPI_BACKEND_URL}/api/stickers?${stringify(
+          {
+            populate: ['image', 'randomVariants'],
+            pagination: {
+              page,
+              pageSize: 100,
+            },
+          }
+        )}`,
+        {
+          cache: 'no-cache',
+          headers: {
+            Authorization: `Bearer ${process.env.STRAPI_API_TOKEN}`,
+          },
+        }
+      )
+
+      const strapiData: StrapiApiListResponse<StickerAttributes> =
+        await strapiResponse.json()
+      stickerRecords.push(...strapiData.data)
+
+      if (
+        strapiData.meta.pagination.pageCount > strapiData.meta.pagination.page
+      ) {
+        await getPage(page + 1)
       }
-    )
+    }
+    await getPage(1)
 
-    const strapiData: StrapiApiListResponse<StickerAttributes> =
-      await strapiResponse.json()
-
-    return strapiData
+    return stickerRecords
   } catch (error) {
     throw error
   }
