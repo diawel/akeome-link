f4b4b65381fbd9bd8eda77ebbb528a4cd5a7da80
addtobasic <addtobasic@gmail.com>
2024-11-15 03:09:30 +0900

    fix: 使用していないLinkの削除

--- src/app/create/detail/[id]/page.tsx
+++ src/app/create/detail/[id]/page.tsx
@@ -2,7 +2,6 @@ import { notFound } from 'next/navigation'
 import Detail from '../../../../layouts/Detail'
 import EditDetail from '../../../../layouts/EditDetail'
 import { getCreatedCard } from '../../../../utils/strapi/card'
-import Link from 'next/link'
 
 const Page = async ({ params }: { params: { id: number } }) => {
   const cardResponse = await getCreatedCard(params.id)
