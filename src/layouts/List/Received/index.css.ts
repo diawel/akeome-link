3f009acc9a135e70684863bcf04858b80f042807
Diawel <38136957+diawel@users.noreply.github.com>
2024-11-14 16:53:29 +0900

    add: 空の年賀状表示

--- src/layouts/List/Received/index.css.ts
+++ src/layouts/List/Received/index.css.ts
@@ -1,1 +1,17 @@
+import { style } from '@vanilla-extract/css'
+import { color } from '../../../utils/styleSchema'
+
 export * from '../common.css'
+
+export const noCardContainer = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+export const noCardMessage = style({
+  fontSize: '16px',
+  fontWeight: 'bold',
+  color: color.red[5],
+  padding: '90px 0px 45px 0px',
+})
--- src/layouts/List/Received/index.tsx
+++ src/layouts/List/Received/index.tsx
@@ -11,6 +11,8 @@ import { useState } from 'react'
 import Card from '../../../components/Card'
 import { mediaRecordsToUrlSet } from '../../../utils/strapi/strapiImage'
 import ListLoader from '../../../components/ListLoader'
+import Image from 'next/image'
+import emptyPost from './empty-post.svg'
 
 type ReceivedProps = {
   initialReceivedCards: StrapiApiListResponse<SecureReceivedCardAttributes>
@@ -22,56 +24,71 @@ const Received = ({ initialReceivedCards }: ReceivedProps) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.cardContainer}>
-        {receivedCards.map((receivedCard, index) => {
-          if (
-            !receivedCard.attributes.card ||
-            !receivedCard.attributes.card.data
-          ) {
-            return null
-          }
-          return (
-            <div className={styles.content} key={index}>
-              <Link
-                href={`/receive/detail/${receivedCard.id}`}
-                className={styles.cardLink}
-              >
-                <div className={styles.card}>
-                  <Card
-                    layout={
-                      receivedCard.attributes.card.data.attributes.view.layout
-                    }
-                    background={
-                      receivedCard.attributes.card.data.attributes.view
-                        .background
-                    }
-                    userImages={mediaRecordsToUrlSet(
-                      receivedCard.attributes.card.data.attributes.userImages
-                        .data
-                    )}
-                    maxFormat="small"
-                  />
-                </div>
-                <div className={styles.cardTitle}>
-                  {receivedCard.attributes.card.data.attributes.creatorName}
-                </div>
-              </Link>
-            </div>
-          )
-        })}
-        <ListLoader
-          loadMore={async () => {
-            const nextPageReceivedCards = await getReceivedCards({
-              page: latestMeta.pagination.page + 1,
-            })
-            if (nextPageReceivedCards) {
-              setReceivedCards(receivedCards.concat(nextPageReceivedCards.data))
-              setLatestMeta(nextPageReceivedCards.meta)
+      {receivedCards.length > 0 ? (
+        <div className={styles.cardContainer}>
+          {receivedCards.map((receivedCard, index) => {
+            if (
+              !receivedCard.attributes.card ||
+              !receivedCard.attributes.card.data
+            ) {
+              return null
             }
-          }}
-          offset={20}
-        />
-      </div>
+            return (
+              <div className={styles.content} key={index}>
+                <Link
+                  href={`/receive/detail/${receivedCard.id}`}
+                  className={styles.cardLink}
+                >
+                  <div className={styles.card}>
+                    <Card
+                      layout={
+                        receivedCard.attributes.card.data.attributes.view.layout
+                      }
+                      background={
+                        receivedCard.attributes.card.data.attributes.view
+                          .background
+                      }
+                      userImages={mediaRecordsToUrlSet(
+                        receivedCard.attributes.card.data.attributes.userImages
+                          .data
+                      )}
+                      maxFormat="small"
+                    />
+                  </div>
+                  <div className={styles.cardTitle}>
+                    {receivedCard.attributes.card.data.attributes.creatorName}
+                  </div>
+                </Link>
+              </div>
+            )
+          })}
+          <ListLoader
+            loadMore={async () => {
+              const nextPageReceivedCards = await getReceivedCards({
+                page: latestMeta.pagination.page + 1,
+              })
+              if (nextPageReceivedCards) {
+                setReceivedCards(
+                  receivedCards.concat(nextPageReceivedCards.data)
+                )
+                setLatestMeta(nextPageReceivedCards.meta)
+              }
+            }}
+            offset={20}
+          />
+        </div>
+      ) : (
+        <>
+          <div className={styles.noCardContainer}>
+            <div className={styles.noCardMessage}>
+              友達と年賀状を共有しよう！
+            </div>
+          </div>
+          <div className={styles.noCardContainer}>
+            <Image src={emptyPost} alt="emptyPost"></Image>
+          </div>
+        </>
+      )}
     </div>
   )
 }
