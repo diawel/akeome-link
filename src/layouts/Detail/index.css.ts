f786dc9b2b97ce31ed6f18507454ce8b700a1a62
addtobasic <addtobasic@gmail.com>
2024-11-14 23:32:22 +0900

    fix: UIをデザインに沿ったものに修正

--- src/layouts/Detail/index.css.ts
+++ src/layouts/Detail/index.css.ts
@@ -1,113 +1,77 @@
 import { style } from '@vanilla-extract/css'
 import { color } from '../../utils/styleSchema'
 
-export const postContainer = style({
-  position: 'relative',
-  width: '100%',
-  height: 'auto',
+
+export const control = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: 16,
 })
 
-export const post = style({
-  width: '100%',
-  height: '288px',
-})
-
-export const card = style({
+export const container = style({
   position: 'absolute',
-  marginTop: '180px',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  zIndex: 1,
-  width: '282px',
-  padding: '16px',
 })
 
-export const linkBoxContainer = style({
+export const center = style({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  paddingTop: '39px',
 })
 
-export const linkBoxContent = style({
-  display: 'flex',
-  alignItems: 'center',
-  border: `1px solid ${color.red[5]}`,
-  marginTop: '220px',
-  borderRadius: '8px',
-  overflow: 'hidden',
-  backgroundColor: color.gray[100],
-  width: '326px',
-  height: '46px',
-})
-
-export const linkText = style({
-  flex: 1,
-  padding: '10px 16px',
-  color: color.red[5],
-  fontSize: '13px',
-  textDecoration: 'none',
-  whiteSpace: 'nowrap',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-})
-
-export const copyButton = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundColor: color.red[5],
-  marginTop: '8px',
-  padding: '10px 12px',
-  cursor: 'pointer',
-  color: color.red[5],
-  fontSize: '14px',
+export const title = style({
+  fontSize: '24px',
   fontWeight: 'bold',
-  border: 'none',
+  paddingBottom: '4px'
 })
 
-export const linkInput = style({
-  flex: 1,
-  padding: '10px 16px',
+export const creatorName = style({
   fontSize: '16px',
+})
+
+export const cardSpace = style({
+  padding: '28px 0px',
+})
+
+export const card = style({
+  width: '282px',
+  boxShadow: '0.1px 0.1px 2px rgba(0, 0, 0, 0.3)',
+})
+
+export const primaryButton = style({
+  padding: '10px 24px',
+  backgroundColor: color.red[5],
+  color: color.gray[100],
+  borderRadius: '12px',
+  cursor: 'pointer',
+  border: 'none',
+  fontSize: '16px',
+  fontWeight: 'bold',
+  width: '280px',
+  height: '42px'
+})
+
+export const copyAndEditButton = style({
+  width: '172px',
+  height: '42px',
+  backgroundColor: 'white',
+  borderRadius: '12px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+  border: 'none',
+})
+
+export const deleteButton = style({
+  width: '96px',
+  height: '42px',
+  backgroundColor: 'white',
   color: color.red[5],
-  border: 'none',
-  outline: 'none',
-  backgroundColor: 'transparent',
-  textOverflow: 'ellipsis',
-  fontWeight: 'bold',
-  cursor: 'default',
-  width: '320px',
-})
-
-export const xContainer = style({
-  width: '74px',
-  height: '56px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'white',
-  borderRadius: '8px',
-  cursor: 'pointer',
-})
-
-export const lineContainer = style({
-  width: '74px',
-  height: '56px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: 'white',
-  borderRadius: '8px',
-  cursor: 'pointer',
-})
-
-export const downloadContainer = style({
-  width: '145px',
-  height: '56px',
-  backgroundColor: 'white',
-  borderRadius: '8px',
+  borderRadius: '12px',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
@@ -115,26 +79,13 @@ export const downloadContainer = style({
   border: 'none',
 })
 
-export const downloadContent = style({
-  display: 'flex',
-  alignItems: 'center',
-})
-
-export const downloadText = style({
-  fontSize: '13px',
+export const buttonText = style({
   fontWeight: 'bold',
-  paddingLeft: '4px',
-})
-
-export const iconButtonContainer = style({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  paddingTop: '25px',
+  fontSize: '16px',
 })
 
 export const buttonSpace = style({
-  padding: '0 15px',
+  paddingRight: '13px',
 })
 
 export const buttonContainer = style({
@@ -143,24 +94,4 @@ export const buttonContainer = style({
   alignItems: 'center',
   paddingTop: '32px',
   cursor: 'pointer',
-})
-
-export const buttonContent = style({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: color.red[5],
-  width: '127px',
-  height: '42px',
-  borderRadius: '12px',
-})
-
-export const buttonText = style({
-  fontSize: '16px',
-  color: 'white',
-  fontWeight: 'bold',
-})
-
-export const cardLink = style({
-  textDecoration: 'none',
 })
--- src/layouts/Detail/index.tsx
+++ src/layouts/Detail/index.tsx
@@ -1,25 +1,19 @@
 'use client'
 
 import Card from '../../components/Card'
-import postImage from './post.svg'
-import xIcon from './icon-x.svg'
-import lineIcon from './icon-line.svg'
-import downloadIcon from './icon-download.svg'
-import Image from 'next/image'
 import * as styles from './index.css'
-import copyIcon from './icon-copy.svg'
 import Link from 'next/link'
 import { mediaRecordsToUrlSet } from '../../utils/strapi/strapiImage'
-import Renderer from '../../components/Card/Renderer'
 import { useEffect, useState } from 'react'
 import { CardAttributes } from '../../utils/strapi/card'
+import ReturnButton from '../../components/ReturnButton'
+import { color } from '../../utils/styleSchema'
 
 type DetailProps = {
   cardAttributes: CardAttributes
 }
 
 const Detail = ({ cardAttributes }: DetailProps) => {
-  const [renderedImage, setRenderedImage] = useState<Blob | null>(null)
   const [shareUrl, setShareUrl] = useState('')
   useEffect(() => {
     setShareUrl(
@@ -29,96 +23,52 @@ const Detail = ({ cardAttributes }: DetailProps) => {
   return (
     <>
       <div>
-        <div className={styles.postContainer}>
-          <Image className={styles.post} src={postImage} alt="postImage" />
-          <div className={styles.card}>
-            <Card
-              layout={cardAttributes.view.layout}
-              background={cardAttributes.view.background}
-              userImages={mediaRecordsToUrlSet(cardAttributes.userImages.data)}
-              randomVariants="hidden"
-            />
-          </div>
+        <div className={styles.control}>
+            <ReturnButton href="/create/list" color={color.gray[5]} />
         </div>
 
-        <div className={styles.linkBoxContainer}>
-          <div className={styles.linkBoxContent}>
-            <input
-              type="text"
-              value={shareUrl}
-              readOnly
-              className={styles.linkInput}
-            />
-            <button
-              className={styles.copyButton}
-              onClick={() => {
-                navigator.clipboard.writeText(shareUrl)
-                alert('コピーしました')
-              }}
-            >
-              <Image src={copyIcon} alt="copyIcon"></Image>
-            </button>
+        <div className={styles.container}>
+          <div className={styles.center}>
+            <div className={styles.title}>{cardAttributes.title}</div>
           </div>
-        </div>
-        <div>
-          <div className={styles.iconButtonContainer}>
-            <Link
-              className={styles.xContainer}
-              href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
-                shareUrl
-              )}&text=年賀状が届いています #あけおめリンク`}
-            >
-              <Image src={xIcon} alt="xIcon"></Image>
-            </Link>
+          <div className={styles.center}>
+            <div className={styles.creatorName}>{cardAttributes.creatorName}</div>
+          </div>
+          <div className={styles.cardSpace}>
+            <div className={styles.card}>
+              <Card
+                layout={cardAttributes.view.layout}
+                background={cardAttributes.view.background}
+                userImages={mediaRecordsToUrlSet(
+                  cardAttributes.userImages.data
+                )}
+                randomVariants="hidden"
+              />
+            </div>
+          </div>
+          <button
+            className={styles.primaryButton}
+            // onClick={}
+          >
+            SNSで共有する
+          </button>
+          <div className={styles.buttonContainer}>
             <div className={styles.buttonSpace}>
-              <Link
-                className={styles.lineContainer}
-                href={`https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(
-                  shareUrl
-                )}&text=年賀状が届いています`}
+              <button
+                className={styles.copyAndEditButton}
+                // onClick={() => {}}
               >
-                <Image src={lineIcon} alt="lineIcon"></Image>
-              </Link>
+                <div className={styles.buttonText}>コピーして編集</div>
+              </button>
             </div>
             <button
-              className={styles.downloadContainer}
-              onClick={() => {
-                if (!renderedImage) return
-                const url = URL.createObjectURL(renderedImage)
-                const a = document.createElement('a')
-                a.href = url
-                a.setAttribute('download', `new_year_card_${Date.now()}`)
-                document.body.appendChild(a)
-                a.click()
-                URL.revokeObjectURL(url)
-                a.remove()
-              }}
-              style={{
-                pointerEvents: renderedImage ? 'auto' : 'none',
-                opacity: renderedImage ? 1 : 0.5,
-              }}
+              className={styles.deleteButton}
             >
-              <div className={styles.downloadContent}>
-                <Image src={downloadIcon} alt="downLoadIcon"></Image>
-                <div className={styles.downloadText}>画像として保存</div>
-              </div>
+              <div className={styles.buttonText}>削除</div>
             </button>
           </div>
-          <Link href="/create/list" className={styles.cardLink}>
-            <div className={styles.buttonContainer}>
-              <div className={styles.buttonContent}>
-                <div className={styles.buttonText}>一覧に戻る</div>
-              </div>
-            </div>
-          </Link>
         </div>
       </div>
-      <Renderer
-        layout={cardAttributes.view.layout}
-        background={cardAttributes.view.background}
-        userImages={mediaRecordsToUrlSet(cardAttributes.userImages.data)}
-        onRender={setRenderedImage}
-      />
     </>
   )
 }
