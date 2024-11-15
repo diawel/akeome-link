0932fa2a097ef66fb152fb6b073fe6a416f257f1
addtobasic <addtobasic@gmail.com>
2024-11-16 02:55:23 +0900

    fix: Detailのレスポンシブ化

--- src/layouts/Detail/index.css.ts
+++ src/layouts/Detail/index.css.ts
@@ -1,36 +1,45 @@
 import { style } from '@vanilla-extract/css'
 import { color } from '../../utils/styleSchema'
 
+export const screen = style({
+  width: '100%',
+  minHeight: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+})
 
 export const control = style({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
   padding: 16,
+  width: '100%',
 })
 
-export const center = style({
+export const container = style({
   display: 'flex',
+  width: '100%',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
+  gap: 28,
+  flex: 1,
+  padding: '32px 56px 64px',
 })
 
 export const title = style({
   fontSize: '24px',
   fontWeight: 'bold',
-  paddingBottom: '4px'
+  paddingBottom: '4px',
 })
 
 export const creatorName = style({
   fontSize: '16px',
 })
 
-export const cardSpace = style({
-  padding: '28px 0px',
-})
-
 export const card = style({
-  width: '282px',
+  width: '100%',
   boxShadow: '0.1px 0.1px 2px rgba(0, 0, 0, 0.3)',
 })
 
@@ -41,15 +50,22 @@ export const primaryButton = style({
   borderRadius: '12px',
   cursor: 'pointer',
   border: 'none',
+  textDecoration: 'none',
   fontSize: '16px',
   fontWeight: 'bold',
-  width: '280px',
-  height: '42px'
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 })
 
-export const copyAndEditButton = style({
-  width: '172px',
-  height: '42px',
+export const link = style({
+  textDecoration: 'none',
+})
+
+export const editButton = style({
+  padding: 10,
+  width: '100%',
   backgroundColor: 'white',
   borderRadius: '12px',
   display: 'flex',
@@ -60,8 +76,8 @@ export const copyAndEditButton = style({
 })
 
 export const deleteButton = style({
-  width: '96px',
-  height: '42px',
+  padding: 10,
+  width: '100%',
   backgroundColor: 'white',
   color: color.red[5],
   borderRadius: '12px',
@@ -77,17 +93,9 @@ export const buttonText = style({
   fontSize: '16px',
 })
 
-export const buttonSpace = style({
-  paddingRight: '13px',
-})
-
-export const link = style({
-  textDecoration: 'none',
-})
-
 export const buttonContainer = style({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '32px 0px',
+  display: 'grid',
+  width: '100%',
+  gridTemplateColumns: '7fr 4fr',
+  gap: 14,
 })
